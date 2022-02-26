0944a7f6d1d056e08e0ce6d3c2419ed218c0bb60
MrPedroGabriel123 <joaofelipechannel0@gmail.com>
2022-02-26 18:42:47 +0000

    step 2.30

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -7,27 +7,28 @@
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("Welcome to calculator!");
             Console.WriteLine("What is your first number?");
-            var first = Console.ReadLine();
+            double first = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("What is your second number?");
-            var second = Console.ReadLine();
+            double second = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("What will the operation be? (+ / -  *) ");
-           string operation = Console.ReadLine()!;
-            if (operation == "+")
+            string operation = Console.ReadLine()!;
+            
+           if (operation == "+")
             {
-                Console.WriteLine($"Your Result: {first} + {second}= ");
+                Console.WriteLine("Your Result: {0}", (first + second));
             }
             if (operation == "-")
             {
-                Console.WriteLine($"Your Result: {first} - {second}= ");
+                Console.WriteLine("Your Result: {0}", (first - second));
             }
             if (operation == "*")
             {
-                Console.WriteLine($"Your Result: {first} * {second}= ");
+                Console.WriteLine("Your Result: {0}", (first * second));
 
             }
             if (operation == "/")
             {
-                Console.WriteLine($"Your Result: {first} / {second}=");
+                Console.WriteLine("Your Result: {0}", (first / second));
             }
 
 
